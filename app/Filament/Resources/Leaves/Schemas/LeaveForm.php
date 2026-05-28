315ebe880710f07c79a6e0c7716ceef91953a9a1
Ram Munda <rmunda2420>
2026-05-29 00:05:03 +0530

    Added leave days calcilation logic

--- app/Filament/Resources/Leaves/Schemas/LeaveForm.php
+++ app/Filament/Resources/Leaves/Schemas/LeaveForm.php
@@ -10,6 +10,9 @@ use Filament\Forms\Components\Textarea;
 use App\Models\Employee;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 
 class LeaveForm
 {
@@ -49,12 +52,60 @@ class LeaveForm
 
                 DatePicker::make('from_date')
                     ->required()
-                    ->live(),
+                    ->live()
+                    ->afterStateUpdated(function (Get $get, Set $set) {
+                        // Needs update time to time on company holiday policy
+                        $from = $get('from_date');
+                        $to = $get('to_date');
+
+                        if ($from && $to) {
+
+                            $start = Carbon::parse($from);
+                            $end = Carbon::parse($to);
+
+                            $days = 0;
+
+                            while ($start->lte($end)) {
+
+                                if (!$start->isWeekend()) {
+                                    $days++;
+                                }
+
+                                $start->addDay();
+                            }
+
+                            $set('days', $days);
+                        }
+                    }),
 
                 DatePicker::make('to_date')
                     ->required()
                     ->afterOrEqual('from_date')
-                    ->live(),
+                    ->live()
+                    ->afterStateUpdated(function (Get $get, Set $set) {
+                        // Needs update time to time on company holiday policy
+                        $from = $get('from_date');
+                        $to = $get('to_date');
+
+                        if ($from && $to) {
+
+                            $start = Carbon::parse($from);
+                            $end = Carbon::parse($to);
+
+                            $days = 0;
+
+                            while ($start->lte($end)) {
+
+                                if (!$start->isWeekend()) {
+                                    $days++;
+                                }
+
+                                $start->addDay();
+                            }
+
+                            $set('days', $days);
+                        }
+                    }),
 
                 TextInput::make('days')
                     ->numeric()
